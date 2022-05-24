fa12de422a77837669358a01338fe0fb4e082161
Jnius0 <rjnius@gmail.com>
2022-05-24 12:01:39 +0200

    Ajout des events de calendrier

--- Dawaes/src/app/one-week-calendar/one-week-calendar.component.ts
+++ Dawaes/src/app/one-week-calendar/one-week-calendar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { CalendarView } from 'angular-calendar';
+import { CalendarView, CalendarEvent } from 'angular-calendar';
+import { startOfDay } from 'date-fns';
 
 @Component({
   selector: 'app-one-week-calendar',
@@ -11,6 +12,12 @@ export class OneWeekCalendarComponent implements OnInit {
   viewDate: Date = new Date();
   view: CalendarView = CalendarView.Month;
   CalendarView = CalendarView;
+  events: CalendarEvent[] = [
+    {
+      start: startOfDay(new Date()),
+      title: 'An event with no end date',
+    }
+  ]
 
   constructor() { }
 
