a0acbb04931b70f07dae317df21c357ff9c157a0
Clamore <rotuman.lenoob@gmail.com>
2022-06-18 23:14:20 +0200

    add observable comment from firestore

--- Dawaes/src/app/liste-commentaires/liste-commentaires.component.ts
+++ Dawaes/src/app/liste-commentaires/liste-commentaires.component.ts
@@ -1,5 +1,8 @@
 import { ObtainCommentairesService } from './../obtain-commentaires.service';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Injector, Output } from '@angular/core';
+import { collection, onSnapshot, query, where } from 'firebase/firestore';
+import { FirebaseService } from '../services/firebase.service';
+import { getAuth } from 'firebase/auth';
 
 @Component({
   selector: 'app-liste-commentaires',
@@ -8,23 +11,35 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ListeCommentairesComponent implements OnInit {
 
-  commentaires:string[]=[]
+  comment:string[]=[]
   @Input() titre?: string
 
   //est lié soit à un utilisateur soit à un établissement:
   @Input() estUtilisateur!:boolean
   @Input() id!:number
 
-  constructor(private service:ObtainCommentairesService) {
-    
-   }
+  @Output() isLogout = new EventEmitter<void>()
+  constructor(private service:ObtainCommentairesService, public firebaseService:FirebaseService, private injector: Injector) {
+    const db = this.injector.get('A');
+  }
 
   ngOnInit(): void {
-    if(this.estUtilisateur){
+    /*if(this.estUtilisateur){
       this.commentaires = this.service.getCommentairesUtilisateur(this.id)
     } else {
       this.commentaires = this.service.getCommentairesEtablissement(this.id)
-    }
+    }*/
+    const auth = getAuth()
+    const user = auth.currentUser;
+    const q = query(collection(this.injector.get('A'), "Comment"), where("Etablissement", "==", "TODO"));
+    const observable = onSnapshot(q, (querySnapshot) => {
+      this.comment = [];
+      querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          this.comment.push(data['text']);
+      });
+    });
+
   }
 
 }
