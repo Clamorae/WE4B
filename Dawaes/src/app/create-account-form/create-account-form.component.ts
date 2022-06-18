25a77f759f4fa5f88e02ca003a841cf47eff4193
Clamore <rotuman.lenoob@gmail.com>
2022-06-18 17:43:58 +0200

    moove some button

--- Dawaes/src/app/create-account-form/create-account-form.component.ts
+++ Dawaes/src/app/create-account-form/create-account-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Injector, OnInit, Output } from '@angular/core';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import { FirebaseService } from '../services/firebase.service';
 
 @Component({
@@ -21,6 +22,23 @@ export class CreateAccountFormComponent implements OnInit {
     if(this.firebaseService.isLoggedIn){
       console.log("connected");
     }
+  }
+
+  async onGoogle(){
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        const user = result.user;
+        console.log("connected");
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+      });
   }
 
 
