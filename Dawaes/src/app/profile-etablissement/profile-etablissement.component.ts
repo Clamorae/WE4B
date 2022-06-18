39f28c5fef1bb24315162756476df2f8c4be925b
Clamore <rotuman.lenoob@gmail.com>
2022-06-18 18:41:13 +0200

    comment add to database

--- Dawaes/src/app/profile-etablissement/profile-etablissement.component.ts
+++ Dawaes/src/app/profile-etablissement/profile-etablissement.component.ts
@@ -1,6 +1,9 @@
 import { Etablissement } from './../class/Etablissment';
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Injector, OnInit, Output } from '@angular/core';
 import { ObtainEtablissementService } from '../obtain-etablissement.service';
+import { addDoc, collection } from 'firebase/firestore';
+import { FirebaseService } from '../services/firebase.service';
+import { getAuth } from 'firebase/auth';
 
 @Component({
   selector: 'app-profile-etablissement',
@@ -12,13 +15,33 @@ export class ProfileEtablissementComponent implements OnInit {
   etablissement:Etablissement
   login:boolean
 
-  constructor(service: ObtainEtablissementService) {
+  @Output() isLogout = new EventEmitter<void>()
+  constructor(service: ObtainEtablissementService, public firebaseService:FirebaseService, private injector: Injector) {
     //TODO - check le login, récupérer l'id via routage
     this.etablissement = service.getData(1)
     this.login=true
+    const db = this.injector.get('A');
    }
 
   ngOnInit(): void {
+  }
+
+  async addComment(comment:string){
+    try {
+      const auth = getAuth()
+      const user = auth.currentUser;
+      if (user !== null) {
+        const docRef = await addDoc(collection(this.injector.get('A'), "Comment"), {
+          text: comment,
+          User: user.email,
+          Etablissement: "TODO"
+          //ANCHOR maybe add rating
+        });
+      console.log("Document written with ID: ", docRef.id);
+      }
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
   }
 
 }
